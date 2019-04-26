e76626ad70caf11ddd2d2a1c24d51c8f50148f12
Michael McQuade <giraffesyo@gmail.com>
2019-04-26 02:29:28 -0500

    Add enemy movement
    Enemy tracking player

---
+++ destroy_all_daleks/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    Transform player;
+    NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
