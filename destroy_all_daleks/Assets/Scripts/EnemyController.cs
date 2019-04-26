2a0925b03f56152051e5369714d6019d73fa13e3
Michael McQuade <giraffesyo@gmail.com>
2019-04-26 03:22:12 -0500

    Rotate towards player when we're in range

--- destroy_all_daleks/Assets/Scripts/EnemyController.cs
+++ destroy_all_daleks/Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     Transform player;
     NavMeshAgent nav;
     float timeSinceLastShot = 0.0f;
+    public float rotationSpeed = .01f;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,10 +23,26 @@ public class EnemyController : MonoBehaviour
         nav.SetDestination(player.position);
         timeSinceLastShot += Time.deltaTime;
         // if we're within 8 units of the player, and its been 2 seconds since our last shot, shoot the player
-        if(timeSinceLastShot > 2.0f && nav.remainingDistance <= 8.2f) {
-            Debug.Log("We are within range, shooting player");
-            timeSinceLastShot = 0.0f;
+        if (nav.remainingDistance <= 8.2f)
+        {
+            LookAtPlayer();
+
+            if (timeSinceLastShot > 2.0f)
+            {
+                Debug.Log("We are within range, shooting player");
+                timeSinceLastShot = 0.0f;
+            }
         }
 
+
     }
+
+    void LookAtPlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+
+    }
+
 }
