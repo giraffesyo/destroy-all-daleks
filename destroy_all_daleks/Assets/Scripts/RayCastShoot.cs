714735f2b5e5d159e99f285fd97939db8d9543ed
Michael McQuade <giraffesyo@gmail.com>
2019-04-26 09:47:43 -0500

    Add character shooting (no damage yet)

---
+++ destroy_all_daleks/Assets/Scripts/RayCastShoot.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastShoot : MonoBehaviour
+{
+
+    public float fireRate = .25f;
+    public float range = 50;
+    public ParticleSystem smokeParticles;
+    public GameObject hitParticles;
+    public GameObject shootFlare;
+    public int damage = 1;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private LineRenderer lineRenderer;
+    private WaitForSeconds shotLength = new WaitForSeconds(.07f);
+    private AudioSource source;
+    private float nextFireTime;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        source = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(.5f, .5f, 0));
+
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, range))
+            {
+                //IDamageable dmgScript = hit.collider.gameObject.GetComponent<IDamageable>();
+                //if (dmgScript != null)
+                //{
+                //    dmgScript.Damage(damage, hit.point);
+                //}
+
+                //if (hit.rigidbody != null)
+                //{
+                //    hit.rigidbody.AddForce(-hit.normal * 100f);
+                //}
+
+                lineRenderer.SetPosition(0, gunEnd.position);
+                lineRenderer.SetPosition(1, hit.point);
+                Instantiate(hitParticles, hit.point, Quaternion.identity);
+            }
+            StartCoroutine(ShotEffect());
+        }
+
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        lineRenderer.enabled = true;
+        source.Play();
+        smokeParticles.Play();
+        shootFlare.SetActive(true);
+        yield return shotLength;
+        lineRenderer.enabled = false;
+        shootFlare.SetActive(false);
+    }
+}
