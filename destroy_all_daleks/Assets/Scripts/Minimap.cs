f0eff7204249fb74c0d621863baddab0a896c0bc
JarethHarmon <48105910+JarethHarmon@users.noreply.github.com>
2019-04-26 23:04:51 -0500

    forgot script to lock rotation

---
+++ destroy_all_daleks/Assets/Scripts/Minimap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform minimap;
+    private void LateUpdate()
+    {
+        minimap.rotation = Quaternion.Euler(new Vector3(-90, 0, 180));
+    }
+}
