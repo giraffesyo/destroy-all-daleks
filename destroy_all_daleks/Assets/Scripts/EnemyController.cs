0be9f858b9cea3b89ff0358639265f81b874ee93
JarethHarmon <48105910+JarethHarmon@users.noreply.github.com>
2019-04-26 19:50:20 -0500

    Textured dalek, made dalek shoot, update DAD dalek
    added textures, can be changed to other colors with albedo
    added laser to dalek, shoots at player , added variable for distance, reload speed, and how long it takes laser to fade
    laser currently looks bad
    updated DAD dalek to reflect changes made by michael

--- destroy_all_daleks/Assets/Scripts/EnemyController.cs
+++ destroy_all_daleks/Assets/Scripts/EnemyController.cs
@@ -7,34 +7,17 @@ public class EnemyController : MonoBehaviour
 {
     Transform player;
     NavMeshAgent nav;
-    float timeSinceLastShot = 0.0f;
     public float rotationSpeed = .01f;
 
-    // Start is called before the first frame update
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         nav = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         nav.SetDestination(player.position);
-        timeSinceLastShot += Time.deltaTime;
-        // if we're within 8 units of the player, and its been 2 seconds since our last shot, shoot the player
-        if (nav.remainingDistance <= 8.2f)
-        {
-            LookAtPlayer();
-
-            if (timeSinceLastShot > 2.0f)
-            {
-                Debug.Log("We are within range, shooting player");
-                timeSinceLastShot = 0.0f;
-            }
-        }
-
-
     }
 
     void LookAtPlayer()
@@ -44,5 +27,4 @@ public class EnemyController : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
 
     }
-
 }
---
+++ destroy_all_daleks/Assets/Scripts/Laser.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    LineRenderer line;
+    public int shotDistance = 10;
+    bool canShoot = false;
+    bool reloaded = true;
+    public float reloadTime = 2.0f;
+    public float fadeTime = 0.1f;
+    Transform player;
+
+    void Awake()
+    {
+        line = GetComponentInChildren<LineRenderer>();
+        line.enabled = false;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        var distance = Vector3.Distance(player.transform.position, this.transform.position);
+
+        if (distance > 7 || reloaded == false)
+        {
+            canShoot = false;
+        }
+        else { canShoot = true;  }
+
+        if (canShoot)
+        {
+            canShoot = false;
+            reloaded = false;
+            StopCoroutine("Shoot");
+            StartCoroutine("Shoot");
+        }
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        line.SetPosition(0, ray.origin);
+        line.SetPosition(1, ray.GetPoint(shotDistance));
+    }
+
+    IEnumerator Shoot()
+    {
+        line.enabled = true;
+        //damagePlayer();
+        yield return new WaitForSeconds(fadeTime);
+        line.enabled = false;
+
+        yield return new WaitForSeconds(reloadTime);
+        reloaded = true;
+    }
+}
